c34d2d8d369a60699ddcdc7569d073c44a4877ae
Georg Reisinger <georg@dergeorg.at>
2021-10-02 15:46:17 +0200

    adding dist subtree

---
+++ dist/precache-manifest.a6743671ba818a8d64463fe539c01e10.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab76848354bb7a1245a",
+    "url": "/Peer-Review-Platform-Frontend/css/ReviewVerfassen.bb80bc29.css"
+  },
+  {
+    "revision": "1e13d9de460d514e0f6c",
+    "url": "/Peer-Review-Platform-Frontend/css/StudentDashboard.d910ed6c.css"
+  },
+  {
+    "revision": "faf845fa48c942848042",
+    "url": "/Peer-Review-Platform-Frontend/css/app.b43d0024.css"
+  },
+  {
+    "revision": "9418eaf59213d9430afd",
+    "url": "/Peer-Review-Platform-Frontend/css/chunk-vendors.f112afbd.css"
+  },
+  {
+    "revision": "28c7adffc9af705dcd8a8b77b1a9c0e8",
+    "url": "/Peer-Review-Platform-Frontend/img/PICA.28c7adff.jpg"
+  },
+  {
+    "revision": "9184c1e0c2158920029fcc243f2fcb9d",
+    "url": "/Peer-Review-Platform-Frontend/index.html"
+  },
+  {
+    "revision": "1b82af8528b519fb3a72",
+    "url": "/Peer-Review-Platform-Frontend/js/OffeneWorkshops.a80e7169.js"
+  },
+  {
+    "revision": "298302d589777f24acb8",
+    "url": "/Peer-Review-Platform-Frontend/js/ReviewOverview.fed6f90f.js"
+  },
+  {
+    "revision": "2ddf18ce26ef8cc2221a",
+    "url": "/Peer-Review-Platform-Frontend/js/ReviewOverview~ReviewVerfassen~WorkshopAbgabe.c24d51f1.js"
+  },
+  {
+    "revision": "fab76848354bb7a1245a",
+    "url": "/Peer-Review-Platform-Frontend/js/ReviewVerfassen.176c8492.js"
+  },
+  {
+    "revision": "1e13d9de460d514e0f6c",
+    "url": "/Peer-Review-Platform-Frontend/js/StudentDashboard.7c54a860.js"
+  },
+  {
+    "revision": "7d1dcb41158317d68746",
+    "url": "/Peer-Review-Platform-Frontend/js/WorkshopAbgabe.c5f407e5.js"
+  },
+  {
+    "revision": "faf845fa48c942848042",
+    "url": "/Peer-Review-Platform-Frontend/js/app.e8ede73f.js"
+  },
+  {
+    "revision": "9418eaf59213d9430afd",
+    "url": "/Peer-Review-Platform-Frontend/js/chunk-vendors.17185f0e.js"
+  },
+  {
+    "revision": "b8b9df90ea0ab88a938ea8dcbb394a01",
+    "url": "/Peer-Review-Platform-Frontend/logo.png"
+  },
+  {
+    "revision": "62f44d97def095f8a899561eb0dfbaf1",
+    "url": "/Peer-Review-Platform-Frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Peer-Review-Platform-Frontend/robots.txt"
+  }
+]);
